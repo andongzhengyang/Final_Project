f921215b95c2f701cae6322a2385822dfe9f42fa
Kara Higgins <kah@mail.harvard.edu>
2017-11-27 12:34:02 -0500

    Changes from meeting on 11/27

--- Project_Code.R
+++ Project_Code.R
@@ -18,8 +18,9 @@ library(lubridate)
 library(stringr)
 library(ggplot2)
 library(ggthemes)
+library(ggrepel)
 library(dslabs)
-ds_theme_set(new="theme_fivethirtyeight")
+ds_theme_set(new="theme_classic")
 
 #Import data
 #Currently using 13NOV2017 freeze
@@ -42,6 +43,7 @@ data <- read.csv(data_location, stringsAsFactors = F)
 #   COLLECTOR: Kara/Ray. Format=factor w/ 2 levels
 #   AM_PM: AM/PM request time. Format=Factor w/ 2 levels, need to derive\
 #   COST_PER_MIN: Cost in dollars per minute of estimated duration. Format=numeric
+#   TIME_FROM_MID: Time from midpoint of rush hour (midpoint=9am morning, 6pm evening)
 
 data2 <- data %>% mutate(DATE=as.Date(DATE, format="%m/%d/%Y"),
                          DAY_OF_WK=as.factor(DAY_OF_WK),
@@ -52,7 +54,8 @@ data2 <- data %>% mutate(DATE=as.Date(DATE, format="%m/%d/%Y"),
                          ARRIVAL_TIME=ARRIVAL_DATETIME-as.POSIXct(paste(DATE,"00:00:00"), format="%Y-%m-%d %H:%M"),
                          COLLECTOR=as.factor(COLLECTOR),
                          AM_PM=as.factor(ifelse(TIME>8 & TIME<10, "AM", "PM")),
-                         COST_PER_MIN=COST/as.numeric(TOTAL_DURATION)
+                         COST_PER_MIN=COST/as.numeric(TOTAL_DURATION),
+                         TIME_FROM_MID=ifelse(AM_PM=="AM",TIME-9, TIME-18)
                            )
 
 #STEP 2
@@ -95,13 +98,15 @@ p + geom_histogram(aes(COST,..density.., fill=SERVICE), color="black", binwidth 
   ggtitle("Distribution of Cost by Service and AM/PM Rush")+
   theme(legend.position = "none")
 
-##Ride request time distribution BY COLLECTOR - graph shows all-day
-p + geom_histogram(aes(as.numeric(TIME),..density..),breaks=seq(8,19,.5),color="black") +
-  geom_vline(xintercept=c(8,10,17,19), lty=2) +
-  geom_label(aes(label="Morning Rush", x=9, y=.6))+
-  geom_label(aes(label="Evening Rush", x=18, y=.6))+
-  ggtitle("Distribution of Ride Request Time (24 hr clock)") +
-  facet_wrap(~COLLECTOR, dir="v")
+#Ride cost - boxplot
+# Calculate medians to label plot
+p_med_cost <- data2 %>% group_by(SERVICE, AM_PM) %>% summarise(med_cost=median(COST))
+# Make plot
+p + geom_boxplot(aes(SERVICE, COST, fill=SERVICE)) +
+  geom_text(data = p_med_cost, aes(x = SERVICE, y = med_cost, label = med_cost), size = 3, vjust = -1)+
+  facet_wrap(~AM_PM) +
+  ggtitle("Distribution of Total Cost by Service and AM/PM Rush")+
+  theme(legend.position = "none")
 
 #Cost/min - histogram
 p + geom_histogram(aes(COST_PER_MIN,..density.., fill=SERVICE), color="black", binwidth=.1) +
@@ -142,24 +147,40 @@ p + geom_boxplot(aes(SERVICE, TOTAL_DURATION, fill=SERVICE)) +
   theme(legend.position = "none")
 
 #Spearman and Mann-Whitney U
+
+#Subset data into AM/PM for stratified analyses
+data2_AM <- data2 %>% filter(AM_PM=="AM")
+data2_PM <- data2 %>% filter(AM_PM=="PM")
+
 #Spearman -> cor(x,y,method="spearman")
 #Testing whether total ride duration and total cost are correlated
-sp <- cor.test(data2$TOTAL_DURATION, data2$COST, method="spearman", exact=F)
-print(sp)
+sp_dur_cost <- cor.test(data2$TOTAL_DURATION, data2$COST, method="spearman", exact=F)
+print(sp_dur_cost)
 
 #Testing whether wait time and total cost are correlated
 sp_wait_cost <- cor.test(data2$WAIT_TIME, data2$COST, method="spearman", exact=F)
 print(sp_wait_cost)
 
+#Testing whether wait time and total cost/min are correlated
+sp_wait_costmin <- cor.test(data2$WAIT_TIME, data2$COST_PER_MIN, method="spearman", exact=F)
+print(sp_wait_costmin)
+
 #Mann-Whitney - Lyft comes first in the list of factors so less would test if Lyft costs less than Uber
+#Tests prob that a randomly selected Uber price is greater than a randomly selected Lyft price
 wilcox.test(data2$COST~data2$SERVICE, alternative="less", exact=F)
+#Same test just for AM
+wilcox.test(data2_AM$COST~data2_AM$SERVICE, alternative="less", exact=F)
+#Same test just for PM
+wilcox.test(data2_PM$COST~data2_PM$SERVICE, alternative="less", exact=F)
+
+#Tests prob that a randomly selected Uber cost/min is greater than a rand sel Lyft cost/min
 wilcox.test(data2$COST_PER_MIN~data2$SERVICE, alternative="less", exact=F)
 
 #Read in weather data
+#Ideally would have 15 min intervals for temp and precip
+#On hold for now - try again later
 library(weatherData)
 w <- checkDataAvailability("KMABOSTO198", "2017-11-20")
-
-
 
 #STEP 3
 #Exploratory continued...
@@ -170,21 +191,20 @@ w <- checkDataAvailability("KMABOSTO198", "2017-11-20")
 
 # Plot price over time
 
-library(ggrepel)
+#Plot of cost vs total duration, color by service
+p + geom_point(aes(TOTAL_DURATION, COST, color=SERVICE)) +
+  ggtitle("Cost vs Total Duration") +
+  xlab("Cost ($)")+
+  ylab("Total Duration")
 
+#Plot of cost vs total duration, color by total duration
+p + geom_point(aes(TOTAL_DURATION, COST, color=as.numeric(abs(TIME_FROM_MID))))+
+  ggtitle("Cost vs Total Duration") +
+  xlab("Cost ($)")+
+  ylab("Total Duration")+
+  facet_wrap(~AM_PM)
 
-#ADD COMMENTS FOR EVERYTHING
-p2 <- data2 %>% ggplot()
-
-#Plot of total duration vs cost
-p2 + geom_point(aes(TOTAL_DURATION, COST))
-
-#Same plot, but adding color for SERVICE
-p2 + geom_point(aes(TOTAL_DURATION, COST, color=SERVICE))
-
-p2 + geom_point(aes(TOTAL_DURATION, COST, color=as.numeric(TIME)))
-
-p2 + geom_point(aes(TOTAL_DURATION, COST, color=SERVICE)) +
+p + geom_point(aes(TOTAL_DURATION, COST, color=SERVICE)) +
   facet_grid(AM_PM~.)
 
 #AM only
@@ -201,10 +221,10 @@ data2 %>% filter(AM_PM=="PM") %>% ggplot() +
 # Havent gone through this together yet
 ###################################################
 
-p2 + geom_abline(intercept = log10(COST_PER_MIN), lty = 2, color = "darkgrey") +
+p + geom_abline(intercept = log10(COST_PER_MIN), lty = 2, color = "darkgrey") +
   geom_point(aes(col=TIME), size = 3)  
 
-p <- p + scale_color_discrete(name = "time1") 
+p2 <- p + scale_color_discrete(name = "time1") 
 
 p + theme_economist()
 
@@ -236,35 +256,6 @@ p + geom_point(size = 3, color ="blue")
 p + geom_point(aes(col=time1), size = 3)
 
 
-
-#t test
-a <- 1:40
-b <- a[seq(1, length(a), 2)]
-b
-# t test for total duration
-
-a <- data2$TOTAL_DURATION[1:39]
-b <- a[seq(1, length(a), 2)]
-b
-
-c <- data2$TOTAL_DURATION[2:40]
-d <- a[seq(2, length(a), 2)]
-d 
-
-t.test(b,d) 
-
-# t test for cost 
-
-a <- data2$COST[1:39]
-b <- a[seq(1, length(a), 2)]
-b
-
-c <- data2$COST[2:40]
-d <- a[seq(2, length(a), 2)]
-d 
-
-
-t.test(b,d) 
 
 #More analysis ideas:
 #Research Q for website: How much $ do you lose by not knowing about the cheaper service
@@ -469,30 +460,21 @@ USAMap +
 
 
 # boston rush hour
-install.packages('ggmap') 
-install.packages('Rcpp')
-install.packages('sp')
-data(FREEZE_13NOV2017)
-qmap("boston", zoom = 13)
+library(ggmap) 
+library(Rcpp)
+library(sp)
 
-bosotnMap <- qmap("boston", zoom = 14)  #First, get a map of Houston
+bostonMap <- qmap("boston", zoom = 12)  #First, get a map of Boston
 
-bostonMap+                                        ##This plots the Houston Map
-  geom_point(aes(x = COST, y = TOTAL_DURATION), data = FREEZE_13NOV2017)    ##This adds the points to it
-
+#Need to pull X and Y from a dataset of longitude and latitude if we want multiple points
 bostonMap+
-  geom_point(aes(x = COST, y = TOTAL_DURATION, colour = WAIT_TIME),
-             data = FREEZE_13NOV2017)
-bostonMap+
-  geom_point(aes(x = COST, y = TOTAL_DURATION, colour = WAIT_TIME),
-             data = FREEZE_13NOV2017)+
-  ggtitle("Map of Rush Hour in Boston")
-
+  geom_point(aes(x = lon, y = lat), data = data2, size=6) ##This adds the points to it
 dev.off()  ##This command indicates that we're done creating our plot.  It finalizes and closes the .png file.
 
-geocode('107 Avenue Louis Pasteur, Boston, MA 02115')
-
-geocode('1 Harvard Yard, Cambridge, MA 02138')
+#Combine to form one dataset
+vand <- geocode("107 Avenue Louis Pasteur Boston, MA 02115")
+widener <- geocode("1 Harvard Yard Cambridge, MA 02138")
+#There should be a function that lets you stack these to form one dataset
 
 ###heatmap
 
